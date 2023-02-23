cc3c3b1d4413173ac83d3ec6e5386f96063bc50e
marcoavfcc01121979 <marcoavfcc@gmail.com>
2023-02-23 17:20:41 -0300

    iniciando a tela de login

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "../../styles/globals.scss";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,27 @@
+// import styles from "../../styles/Home.module.scss";
+
+import Head from "next/head";
+import styles from "../../styles/Home.module.scss";
+import Image from "next/image";
+import logoImg from "../../public/logo.svg";
+import { Input } from "../components/ui/input";
+
 export default function Home() {
   return (
     <>
-      <h1>Marco</h1>
+      <Head>
+        <title>Pizzaria - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Pizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
